1299024b46a3b2aad784b3d75b4d56c7d0dfcc0d
Eeshan1608 <sree.eeshan@gmail.com>
2025-09-18 01:56:15 +0000

    Added Rock paper scissors V2

--- main.py
+++ main.py
@@ -1,56 +1,106 @@
 
-# # import necessary libraries
-# #import getpass
-# # use getpass for hidden input
-# # use getpass for hidden input
-# #secret_word = getpass.getpass("Enter your word:").lower()
-# #hint = input("Enter a hint: ")
-# #guessed_letters = []
-# #wrong_guesses = 0
-# #max_wrong = 5
-# #display = ""
-
-# # fix the error in the condition
-# #while wrong_guesses < max_wrong:
-#   #  display = ""
-#   #  for letter in secret_word:
-#    #     if letter in guessed_letters:
-#    #         display = display + letter
-#   #      else:
-#          #   display += "_"
-
-#   #  print("Word:", display)
-#     print("Hint:", hint)
-
-#     # fix the condition to check if the word has been guessed
-#     if display == secret_word:
-#         print("Congratulations! You guessed the word:", secret_word)
-#         break
 
 
-#     # convert the input to lowercase
-#     guess = input("Enter a letter: ").lower()
 
-#     if guess not in secret_word:
-#         # make sure a life is lost if the guessed letter is not in the word
-#         wrong_guesses = wrong_guesses  + 1
-#         print("Wrong guess! Lives left:", max_wrong - wrong_guesses)
-#         guessed_letters.append(guess)
-#         print(guessed_letters)
+import getpass
+import random
+rules = ""
+user_choice = 0
+user_choice2 = 0
+user_wins2 = 0
+user_wins = 0
+gestures = ["rock", "scissor","paper"]
+options = [1,2,3]
+print("Hello welcome to ROCK SCISSOR PAPER GAME R = 1, S = 2, P = 3") 
+comp_player = input("Press 1 to play with computer or 2 to play with another player: ")
+rules = input("If you don't know the rules type no if you know the rules type yes: ")
+if rules.lower() == "no":
+    print("Rock beats scissor, scissor beats paper, and paper beats rock")
+    print("")
+    print("Rock = 1, Scissor = 2, Paper = 3")
+    print("we play now")
+else:
+    print("ok we play now")
+if int(comp_player) == 1:
+    print("ok we play with computer")
+    print("")
+    
 
-#     else:
-#         print ("Correct guess!")
-#         guessed_letters.append(guess)
-#         print(guessed_letters)
+    while True:
+        user_choice = int(input("Enter your choice: "))
 
-# if display != secret_word:
-#     print("Game over! The word was: ", secret_word)
+        comp_choice = random.choice(options)
+        print("Computer choice is: ",(gestures[comp_choice - 1]))
+        if user_choice - comp_choice == -1 or user_choice - comp_choice == 2:
+            print("You win")
+            user_wins += 1
+            print("Your score is: ", user_wins)
+        elif user_choice == comp_choice:
+            print("It's a tie")
+            user_wins += 0
+            print("your score is: ", user_wins)
 
-# print("")
-# while True:
-#     print ("Do you want to play again?")
-#     playagain = input("If you want play again type yes or no: ").lower()
-#     if playagain == "yes":
-#         continue 
-#     else:
-#         break 
+        else:
+            print("you lost")
+            user_wins -= 1
+            print("your score is: ", user_wins)
+
+
+
+        playagain = input("Do you want to play again? (yes/no): ")
+        if playagain.lower() == "no":
+            print("Your final score is: ", user_wins)
+            break
+else:
+    
+    if rules.lower == "no":
+        print("Rock beats scissor, scissor beats paper, and paper beats rock")
+        print("")
+        print("Rock = 1, Scissor = 2, Paper = 3")
+        print("we play now")
+    else:
+        print("ok we play now")
+    while True:
+        user_choice = int(getpass.getpass("Enter your choice player1: "))
+        user_choice2 = int(getpass.getpass("Enter your choice player 2: "))
+        if user_choice - user_choice2 == -1 or user_choice - user_choice2 ==2:
+            print("player 1 wins")
+            user_wins += 1
+            print("Player 1 score is: ", user_wins)
+            print("Player 2 score is: ", user_wins2)
+        elif user_choice == user_choice2:
+            print("ITS TIE")
+            user_wins += 0
+            user_wins2 += 0
+            print("Player 1 score is: ", user_wins)
+            print("Player 2 score is: ", user_wins2)
+        else:
+            print("Player 2 wins")
+            user_wins2 += 1
+            print("Player 2 score is: ", user_wins2)
+            print("Player 1 score is: ", user_wins)
+
+        playagain = input("Do you want to play again? (yes/no): ")
+        if playagain.lower() == "no":
+            print("Your final score Player 1 is: ", user_wins)
+            print("Your final score is Player 2 is: ", user_wins2)
+            break
+        
+            
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
